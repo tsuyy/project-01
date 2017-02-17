fb6b438a95ecdbd87c33f9d323f7fcf42e4c9445
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-02-17 15:32:24 -0800

    modified mongoose connect

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/immersive-exhibitions");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/immersive-exhibitions");
 
 var Exhibition = require('./exhibition');
 
